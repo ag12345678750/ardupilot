f80133b243cf08b355cd8d014eb6df34e74e44db
Liad <gokhman_alex@yahoo.com>
2022-01-04 16:22:10 +0200

    Update version.h

--- ArduPlane/version.h
+++ ArduPlane/version.h
@@ -2,7 +2,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduPlane V3.8.5.0-tfminmi"
+#define THISFIRMWARE "ArduPlane V3.8.5.0-tfmini"
 #define FIRMWARE_VERSION 3,8,5,FIRMWARE_VERSION_TYPE_OFFICIAL
 
 #ifndef GIT_VERSION
